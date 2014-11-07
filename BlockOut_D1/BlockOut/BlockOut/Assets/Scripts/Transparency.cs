bf5f53e5525edbd13860da847f2d0dd084c0102f
StephanieJurgiel <snj6366@rit.edu>
2014-11-07 17:05:02 -0500

    StephChanges2
    More Models for Living Room Scene

--- BlockOut_D1/BlockOut/BlockOut/Assets/Scripts/Transparency.cs
+++ BlockOut_D1/BlockOut/BlockOut/Assets/Scripts/Transparency.cs
@@ -5,6 +5,11 @@ public class Transparency : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		if (this.gameObject.tag == "window") {
+			Color color1 = this.renderer.material.color;
+			color1.a = 0.9f; 
+			this.renderer.material.color = color1;
+		}
 		Color color2 = this.renderer.material.color;
 		color2.a = 0.5f; 
 		this.renderer.material.color = color2;
